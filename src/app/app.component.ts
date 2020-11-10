ed3fbd24f7cd87dbad3a3d5bc6504d764cd37154
sushil <vangarsushil@gmail.com>
2020-11-11 00:24:39 +0530

    transalte project done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'translate';
+  listhinding = 'Change Language';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'hi', 'mr', 'te']);
+    translate.setDefaultLang('en');
+    translate.use('hi');
+  }
 }
